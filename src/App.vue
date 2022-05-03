3f85a066952113670896dba5f0d439b9fae05083
Phil McCluskey <phil@dontquestion.me>
2022-05-03 12:51:57 -0700

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <TempleDebug />
+</template>
+
+<script lang="ts">
+import TempleDebug from '@/components/TempleDebug.vue'
+export default {
+  components: {
+    TempleDebug
+  }
+}
+</script>
---
+++ src/components/TempleDebug.vue
@@ -0,0 +1,20 @@
+<template>
+    <button @click="connectWallet('temple')">Check Balance</button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { TezosToolkit } from '@taquito/taquito';
+
+export default defineComponent({
+  name: 'TempleDebug',
+  methods: {
+    async connectWallet (wtype: string) {
+      if (wtype === 'temple') {
+        const tezos = new TezosToolkit('https://rpc.tzstats.com');
+        const balance = tezos.tz.getBalance('tz1a8QSHVHVkCmDYio6ptk7LAL7ZvywL6Vjo')
+      }
+    }
+  }
+})
+</script>
